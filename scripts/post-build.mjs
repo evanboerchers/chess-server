9100f0bfbaec43fb941b7dc6e49cb4cdfeccadf9
evankboerchers <evan.boerchers@gmail.com>
2025-04-05 21:41:22 -0700

    fixed docker up

--- scripts/post-build.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'fs-extra'
-import path from 'path'
-const __dirname = import.meta.dirname
-const src = path.resolve(__dirname, '../dist')
-const dest = path.resolve(__dirname, '../deploy', 'dist')
-fs.emptyDirSync(src, dest);
-console.log(`Copied ${src} to ${dest}`)
