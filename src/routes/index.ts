600ec45c0b71dfd9b4282b79bfa4e436dc3c0a82
evankboerchers <evan.boerchers@gmail.com>
2025-04-11 23:35:03 -0700

    added home api endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,8 @@ import { Router } from "express"
 
 const router = Router();
 
+router.get('/', (_req, res) => {
+    res.send("Hello, this is the coffee chess api server")
+})
+
 export default router
